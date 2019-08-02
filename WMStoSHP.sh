75477af05d21da1bde6b8f1d1753ce79a4fffc75
ccancellieri <ccancellieri@hotmail.com>
2019-08-02 14:58:37 +0200

    now we fetch a get capabilities and automatically process all the layers

--- WMStoSHP.sh
+++ WMStoSHP.sh
@@ -66,13 +66,22 @@ echo "<GDAL_WMS>
 
 gdal_translate $out.xml $out.tif
 
+if [ $? -eq 0 ]; then
+  # all checks passed!
+  echo "$out.tif fetched!"
+  rm $out.xml
+else
+  echo "Failed to fetch $out.tif"
+  exit 1
+fi
+
 gdaladdo -r cubic --config COMPRESS_OVERVIEW PNG --config INTERLEAVE_OVERVIEW PIXEL --config BIGTIFF_OVERVIEW IF_NEEDED $out.tif 2 4 8 16
 
 #curl "$url?service=WMS&version=1.1.0&request=GetMap&tiled=true&layers=$layername&bbox=$bbox&width=768&height=451&srs=$srs&format=mbtiles&transparency=true&bgColor=0xFFFFFF&format_options=max_zoom:$MAX_ZOOM;min_zoom:$MIN_ZOOM;tileset_name:$TILESET_NAME;" --max-time $TIMEOUT -o $out.mbtiles
 
 if [ $? -eq 0 ]; then
       # all checks passed!
-      echo "$out.tif fetched!"
+      echo "$out.tif added overviews."
 else
   echo "Failed to fetch $out.tif"
   exit 1
@@ -91,6 +100,7 @@ gdal_calc.py -R $in.tif --R_band=1 -G $in.tif --G_band=2 -B $in.tif --B_band=3 -
 
 if [ $? -eq 0 ]; then
   echo "Succesfully generated $out.tif"
+  rm $in.tif
 else
   echo "Unable to generate $out.tif"
   exit 1
@@ -98,26 +108,28 @@ fi
 
 ## Polygonize (GeoTIFF -> SHP)
 
-in=$out
+#in=$out
 
-gdal_polygonize.py $in.tif -b 1 -f "ESRI Shapefile" $out.shp
+gdal_polygonize.py $out.tif -b 1 -f "ESRI Shapefile" $in.shp
 
 if [ $? -eq 0 ]; then
   echo "Succesfully generated $out.shp"
+  rm $in.tif
 else
   echo "Unable to generate $out.shp"
   exit 1
 fi
 
 ## Simplify (SHP -> SHP)
-in=$out
-m=1
-out=$in"_simplified_"$m
-ogr2ogr -f 'ESRI Shapefile' -simplify $m $out.shp $in.shp
+#in=$out
+#m=1
+#out=$in"_simplified_"$m
+#ogr2ogr -f 'ESRI Shapefile' -simplify $m $out.shp $in.shp
 
-if [ $? -eq 0 ]; then
-  echo "Succesfully simplified $in.shp to $out.shp"
-else
-  echo "Unable to simplify $in.shp"
-  exit 1
-fi
+#if [ $? -eq 0 ]; then
+#  echo "Succesfully simplified $in.shp to $out.shp"
+#else
+#  echo "Unable to simplify $in.shp"
+#  exit 1
+#fi
+
---
+++ run.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ $1 == "-h" ]; then
+  echo $0 GET_CAPABILITIES_URL CRS
+  exit 0
+elif [ $# -lt 2 ]; then
+  echo "Missing params (use -h)"
+  exit -1
+fi
+
+CRS=$2
+
+curl $1 -o "WMSServer_orig.xml"
+
+if [ $? -eq 0 ]; then
+  # all checks passed!
+  echo "GetCapabilities fetched from URL ( $1 )"
+else
+  echo "Failed to fetch WMSServer_orig.xml from URL ( $1 )"
+  exit 1
+fi
+
+sed '1d' WMSServer_orig.xml | sed -e "2 s/xmlns=".*"//g" | sed -e "s/xlink\://g" > WMSServer.xml
+
+if [ $? -eq 0 ]; then
+  # all checks passed!
+  rm WMSServer_orig.xml
+else
+  echo "Failed to process WMSServer_orig.xml"
+  exit 1
+fi
+
+url=`xmllint --format --xpath 'string(//WMS_Capabilities/Service/OnlineResource/@href)' WMSServer.xml`
+
+echo "Base url: $url"
+
+layerSize=`xmllint --format --xpath 'count(//WMS_Capabilities/Capability/Layer/Layer/Title)' WMSServer.xml`
+
+echo "Number of layers: $layerSize"
+
+i=1;
+for (( i=1; i<=$layerSize; i++ )); do
+  layer_title=`xmllint --format --xpath 'string(//WMS_Capabilities/Capability/Layer/Layer['$i']/Title)' WMSServer.xml`
+  layer_name=`xmllint --format --xpath 'string(//WMS_Capabilities/Capability/Layer/Layer['$i']/Name)' WMSServer.xml`
+  echo "Working over layer n. $i : "$layer_name
+  cmd=$(xmllint --format --xpath '//WMS_Capabilities/Capability/Layer/Layer['$i']/BoundingBox[@CRS="'$CRS'"]/@*' WMSServer.xml)
+  #cmd=$(xmllint --format --xpath '//WMS_Capabilities/Capability/Layer/Layer['$i']/BoundingBox[1]/@*' WMSServer.xml)
+  if [ -n "$cmd" ]; then
+    cmd="$cmd url=$url ./WMStoSHP.sh '$layer_name' ${layer_title// /_}"
+    echo "Executing: $cmd"
+    eval $cmd
+  else
+    echo "unable to find selected CRS ($CRS) for this layer! (skipping)"
+  fi
+done
+
+if [ $? -eq 0 ]; then
+  # all checks passed!
+  echo "Job done!"
+  rm WMSServer.xml 
+else
+  echo "Failed to complete all task at layer number $i"
+  exit 1
+fi
+
+exit 0
+
